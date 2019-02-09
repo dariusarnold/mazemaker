ce731cc2dc16b38ff34f001d48f14f05d981de98
Darius Arnold <mail@dariusarnold.de>
2019-02-09 20:54:25 +0100

    Rewrite maze to keep walls as part of cell instead as blocked cells in grid
    This breaks the old plotting function of imshow

--- maze.py
+++ maze.py
@@ -1,6 +1,7 @@
 import argparse
 from collections import namedtuple
 from random import choice, seed
+import enum
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -20,71 +21,102 @@ class Stack:
         return len(self.l)
 
 
-class Cell(namedtuple("Cell", ("x", "y"))):
+class Direction(enum.IntEnum):
+    N = 0
+    E = 1
+    S = 2
+    W = 3
+
+
+class Mapping:
+    direction_to_step = {Direction.N: (0, -1),
+                         Direction.E: (1, 0),
+                         Direction.S: (0, 1),
+                         Direction.W: (-1, 0)}
+
+    step_to_direction = {value: key for key, value in direction_to_step.items()}
+
+
+class CellIndex(namedtuple("Cell", ("x", "y"))):
     def __add__(self, other):
-        return Cell(x=self.x + other[0], y=self.y + other[1])
+        return CellIndex(x=self.x + other[0], y=self.y + other[1])
 
     def __sub__(self, other):
         return self.x - other.x, self.y - other.y
 
 
-class Maze():
+class Cell:
+    def __init__(self):
+        self.visited = False
+        self.walls = [True]*4
+
+    def remove_wall(self, direction):
+        self.walls[direction.value] = False
+
+    def __repr__(self):
+        return f"Cell({self.walls}, visited={self.visited})"
+
+class Maze:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.maze = np.full((height+2, width+2), False)
+        self.width = int(width)
+        self.height = int(height)
+        self.cell_grid = np.array([Cell() for _ in range(height * width)]).reshape((height, width))
 
-    def get_neighbour_cells(self, cell):
-        stencil = ((0, 2), (0, -2), (2, 0), (-2, 0))
-        neighbour_cells = [cell + diff for diff in stencil]
-        neighbour_cells = [cell for cell in neighbour_cells if 0 < cell.y <= self.width and 0 < cell.x <= self.height]
-        neighbour_cells_state = [self.maze[cell] for cell in neighbour_cells]
-        return neighbour_cells, neighbour_cells_state
+    def get_neighbour_cell_indices(self, cell_index):
+        stencil = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        neighbour_cell_indices = [cell_index + diff for diff in stencil]
+        neighbour_cell_indices = [cell_index for cell_index in neighbour_cell_indices if self.index_in_grid(cell_index)]
+        return neighbour_cell_indices
 
-    def get_unvisited_neighbours(self, cell):
-        neighbour_cells, neighbour_cells_state = self.get_neighbour_cells(cell)
-        unvisited_cells = [cell for cell, state in zip(neighbour_cells, neighbour_cells_state) if state == False]
+    def index_in_grid(self, cell_index):
+        return 0 <= cell_index.y < self.width and 0 <= cell_index.x < self.height
+
+    def get_unvisited_neighbours(self, cell_index):
+        neighbour_cell_indices = self.get_neighbour_cell_indices(cell_index)
+        unvisited_cells = [index for index in neighbour_cell_indices if self.get_cell(index).visited == False]
         return unvisited_cells
 
-    def set_visited(self, cell):
-        self.maze[cell] = True
+    def set_visited(self, cell_index):
+        self.get_cell(cell_index).visited = True
 
-    def remove_wall(self, cell, diff):
-        self.maze[cell+diff] = True
+    def remove_walls(self, start_cell_index, end_cell_index):
+        direction_to = Mapping.step_to_direction[end_cell_index - start_cell_index]
+        direction_from = Mapping.step_to_direction[start_cell_index - end_cell_index]
+        self.get_cell(start_cell_index).remove_wall(direction_to)
+        self.get_cell(end_cell_index).remove_wall(direction_from)
 
-    def move(self, start_cell, end_cell):
-        self.set_visited(end_cell)
-        self.remove_wall(start_cell, [d//2 for d in end_cell-start_cell])
+    def move(self, start_cell_index, end_cell_index):
+        self.set_visited(end_cell_index)
+        self.remove_walls(start_cell_index, end_cell_index)
+
+    def get_cell(self, cell_index):
+        return self.cell_grid[cell_index.y, cell_index.x]
 
 
 def has_unvisited_neighbours(maze, cell):
-    neighbour_cells = maze.get_neighbour_cells(cell)
+    neighbour_cells = maze.get_neighbour_cell_indices(cell)
     neighbour_cells = [maze.maze[cell] for cell in neighbour_cells]
     return neighbour_cells
-
 
 
 def generate_maze(width, height):
     maze = Maze(width, height)
     stack = Stack()
-    current_cell = Cell(1, 1)
-    maze.set_visited(current_cell)
-    while not np.all(maze.maze):
-        unvisited_cells = maze.get_unvisited_neighbours(current_cell)
+    current_cell_index = CellIndex(0, 0)
+    maze.set_visited(current_cell_index)
+    while True:
+        unvisited_cells = maze.get_unvisited_neighbours(current_cell_index)
         # if the current cell has any neighbours which have not been visited
         if len(unvisited_cells) > 0:
             # choose one random unvisited neighbour and travel there
-            chosen_cell = choice(unvisited_cells)
-            stack.push(current_cell)
-            maze.move(current_cell, chosen_cell)
-            current_cell = chosen_cell
+            chosen_cell_index = choice(unvisited_cells)
+            stack.push(current_cell_index)
+            maze.move(current_cell_index, chosen_cell_index)
+            current_cell_index = chosen_cell_index
         elif len(stack) > 0:
             # if all neighbours have been visited backtrack to the previous cell
-            current_cell = stack.pop()
+            current_cell_index = stack.pop()
         else:
-            # done, add entry and exit of maze
-            maze.maze[0, 1] = True
-            maze.maze[height, width+1] = True
             break
     plt.imshow(maze.maze, interpolation="None", cmap="gray")
     ax = plt.gca()
