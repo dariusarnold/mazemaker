09357e9198a13bf249917fea29da1e055fdaf6ff
Darius Arnold <mail@dariusarnold.de>
2019-02-10 08:27:35 +0100

    Add mask image to maze and as cmd argument
    The mask image allows to block certain sections. Black cells are forbidden for the algorithm.

--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from collections import namedtuple
 from random import choice, seed
 import enum
@@ -57,12 +58,21 @@ class Cell:
         return f"Cell({self.walls}, visited={self.visited})"
 
 class Maze:
-    def __init__(self, width, height):
+    def __init__(self, width, height, mask=None):
+        """
+        :param width: Number of cells in horizontal direction
+        :param height: Number of cells in vertical direction
+        :param mask: Bool array where False marks cells that are not visited by the algorithm
+        """
         self.width = int(width)
         self.height = int(height)
         self.cell_grid = np.array([Cell() for _ in range(height * width)]).reshape((height, width))
+        self.mask = mask
 
     def get_neighbour_cell_indices(self, cell_index):
+        """
+        Return the indices of all neighbour cells of cell_index that lie within the grid
+        """
         stencil = ((0, 1), (0, -1), (1, 0), (-1, 0))
         neighbour_cell_indices = [cell_index + diff for diff in stencil]
         neighbour_cell_indices = [cell_index for cell_index in neighbour_cell_indices if self.index_in_grid(cell_index)]
@@ -74,6 +84,9 @@ class Maze:
     def get_unvisited_neighbours(self, cell_index):
         neighbour_cell_indices = self.get_neighbour_cell_indices(cell_index)
         unvisited_cells = [index for index in neighbour_cell_indices if self.get_cell(index).visited == False]
+        # TODO inherit from Maze to implement masked maze and override this function
+        if self.mask is not None:
+            unvisited_cells = [index for index in unvisited_cells if self.get_mask(index) == True]
         return unvisited_cells
 
     def set_visited(self, cell_index):
@@ -92,12 +105,18 @@ class Maze:
     def get_cell(self, cell_index):
         return self.cell_grid[cell_index.y, cell_index.x]
 
+    def get_mask(self, cell_index):
+        """Return the mask value at cell_index """
+        return self.mask[cell_index.y, cell_index.x]
+
 
 class MazeVisualizerPIL:
     def __init__(self, maze, cell_size_pixels):
         self.maze = maze
         self.cell_size_pixels = cell_size_pixels
         self._init_plot()
+        self.fill_color = 0
+        self.line_width = 1
 
     def _init_plot(self):
         width = self.maze.width*self.cell_size_pixels+1
@@ -108,30 +127,30 @@ class MazeVisualizerPIL:
     def plot_walls(self):
         for hor_index in range(self.maze.width):
             for ver_index in range(self.maze.height):
-                cell = self.maze.get_cell(CellIndex(x=hor_index, y=ver_index))
                 top_left_pixel = (hor_index*self.cell_size_pixels, ver_index*self.cell_size_pixels)
                 top_right_pixel = ((hor_index+1)*self.cell_size_pixels, ver_index*self.cell_size_pixels)
                 bottom_left_pixel = (hor_index*self.cell_size_pixels, (ver_index+1)*self.cell_size_pixels)
                 bottom_right_pixel = ((hor_index+1)*self.cell_size_pixels, (ver_index+1)*self.cell_size_pixels)
-                fill = 0
-                width = 1
+                if self.maze.get_mask(CellIndex(x=hor_index, y=ver_index)) == False:
+                    self.draw.rectangle((top_left_pixel, bottom_right_pixel), outline=self.fill_color, fill=self.fill_color)
+                cell = self.maze.get_cell(CellIndex(x=hor_index, y=ver_index))
                 if cell.walls[Direction.N]:
-                    self.draw.line((top_left_pixel, top_right_pixel), fill, width)
+                    self.draw.line((top_left_pixel, top_right_pixel), self.fill_color, self.line_width)
                 if cell.walls[Direction.E]:
-                    self.draw.line((top_right_pixel, bottom_right_pixel), fill, width)
+                    self.draw.line((top_right_pixel, bottom_right_pixel), self.fill_color, self.line_width)
                 if cell.walls[Direction.S]:
-                    self.draw.line((bottom_right_pixel, bottom_left_pixel), fill, width)
+                    self.draw.line((bottom_right_pixel, bottom_left_pixel), self.fill_color, self.line_width)
                 if cell.walls[Direction.W]:
-                    self.draw.line((top_left_pixel, bottom_left_pixel), fill, width)
+                    self.draw.line((top_left_pixel, bottom_left_pixel), self.fill_color, self.line_width)
 
     def save_plot(self, filename):
         self.img.save(filename)
 
 
-def generate_maze(width, height, filename):
-    maze = Maze(width, height)
+def generate_maze(width, height, output_filename, mask=None):
+    maze = Maze(width, height) if mask is None else Maze(width, height, mask)
     stack = Stack()
-    current_cell_index = CellIndex(0, 0)
+    current_cell_index = CellIndex(25, 34)
     maze.set_visited(current_cell_index)
     while True:
         unvisited_cells = maze.get_unvisited_neighbours(current_cell_index)
@@ -149,17 +168,28 @@ def generate_maze(width, height, filename):
             break
     plotter = MazeVisualizerPIL(maze, 5)
     plotter.plot_walls()
-    plotter.save_plot(filename)
+    plotter.save_plot(output_filename)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Generate mazes.")
-    parser.add_argument("width", type=int, help="Width of the maze in cells")
-    parser.add_argument("height", type=int, help="Height of the maze in cells")
-    parser.add_argument("-f", "--filename", help="Filename that is used to save the maze", default="maze.png")
-    parser.add_argument("-s", "--seed", help="Seed for random generator")
+    parser.add_argument("width", type=int, help="Width of the maze in cells.")
+    parser.add_argument("height", type=int, help="Height of the maze in cells.")
+    parser.add_argument("-f", "--filename", help="Filename that is used to save the maze.", default="maze.png")
+    parser.add_argument("-m", "--maskimg", default=None, help="Image to use as mask, where only white pixels can be visited by the algorithm, while black pixels are forbidden. If this is specified, the maze will be of the same dimensions as the mask image.")
+    parser.add_argument("-s", "--seed", help="Seed for random generator.")
+    # TODO add start cell argument
     args = parser.parse_args()
 
     seed(args.seed)
 
-    generate_maze(args.width, args.height, args.filename)
+    if args.maskimg is not None:
+        try:
+            img = Image.open(args.maskimg)
+        except IOError:
+            sys.exit("Can't open maskimage.")
+        mask = np.asarray(img, dtype=bool)
+        width, height = img.size
+        generate_maze(width, height, args.filename, mask)
+    else:
+        generate_maze(args.width, args.height, args.filename)
